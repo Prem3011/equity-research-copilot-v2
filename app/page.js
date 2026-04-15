cdfde1d042ca250c26a6a84aee19494bc140d464
Prem Acharya <93860406+Prem3011@users.noreply.github.com>
2026-04-15 17:27:52 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import CreditStrengths from "@/components/CreditStrengths";
 import { useState, useCallback } from "react";
 import Header from "@/components/Header";
 import TickerHero from "@/components/TickerHero";
